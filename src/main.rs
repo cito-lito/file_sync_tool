fc0c3db6fcb2b57c0bc06ba95f2b93bb43bd39e7
Mario Maita <thetechpomelo@gmail.com>
2023-11-09 15:50:09 +0100

    add notify, log file events

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use notify::{RecommendedWatcher, RecursiveMode, Result, Watcher, Config};
+use std::path::PathBuf;
+use std::sync::mpsc::channel;
+
+fn main() -> Result<()> {
+
+    // config  with debounce duration
+    let config = Config::default(); // default polling 2s
+    let path_to_watch = PathBuf::from("./");
+
+    // channel to receive file events
+    let (tx, rx) = channel();
+
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, config)?;
+
+    // Add a path to be watched. All files and directories at that path, recursively.
+    watcher.watch(&path_to_watch, RecursiveMode::Recursive)?;
+
+    // log the file events for now
+    for event in rx {
+        println!("{:?}", event);
+    }
+    Ok(())
 }
