d3c8db1e24942ca0ed532b6fd44d46c91afb821c
Mario Maita <thetechpomelo@gmail.com>
2023-11-14 20:51:18 +0100

    tests

--- tests/file_watcher_test.rs
+++ tests/file_watcher_test.rs
@@ -1,29 +1,28 @@
+use file_sync_tool::path_watcher;
 use std::fs::{self, File};
 use std::io::{self};
 use std::path::PathBuf;
 use std::time::Duration;
-use file_sync_tool::path_watcher;
-
 
 #[test]
 fn test_file_create_event() -> io::Result<()> {
     let test_dir = PathBuf::from("./tests");
     let test_file = test_dir.join("test_file.txt");
 
-    let (_watcher, rx) = path_watcher::watch_path(test_dir.clone()).unwrap();
-    
+    let (_watcher, rx) =
+        path_watcher::watch_path(test_dir.clone()).expect("Failed to start watch path");
+
     // trigger event by creating a file
-    let mut _file: File = File::create(&test_file)?;
-    
+    let mut _file: File = File::create(&test_file).expect("Failed to create test file");
+
     println!("total events: {:?}", rx);
-    
+
     let timeout = Duration::from_secs(3);
     let received_event = rx.recv_timeout(timeout).expect("No event received");
-    
+
     println!("Event: {:?}", received_event);
 
-
-    fs::remove_file(&test_file)?;
+    fs::remove_file(&test_file).expect("Failed to remove test file");
 
     // check that an event was received
     assert!(received_event.is_ok(), "Failed to receive file event");
