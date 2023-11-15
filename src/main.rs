52c6004e21db15b73b2b5514968bf49311c59032
Mario Maita <thetechpomelo@gmail.com>
2023-11-16 00:39:00 +0100

    copy if create file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use notify::{
+    event::{self, CreateKind},
+    Event, EventKind,
+};
 use std::path::PathBuf;
 mod path_watcher;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path_to_watch = PathBuf::from("./");
+    let path_to_watch = PathBuf::from("./tests/from");
+    println!("SOURCE path: {:?}", path_to_watch);
 
     let (_watcher, event_rx) = path_watcher::watch_path(path_to_watch)?;
 
@@ -16,7 +21,50 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn handle_event(event: notify::Event) {
-    //do smth
-    println!("{:?}", event)
+fn handle_event(event: Event) {
+    match event.kind {
+        EventKind::Create(event_kind) => handle_create_event(event_kind, event.paths),
+        EventKind::Modify(_) => {
+            println!("MODIFY {:?}", event);
+        }
+        EventKind::Remove(_) => {
+            println!("REMOVE {:?}", event);
+        }
+        _ => {
+            println!("UNHANDLED{:?}", event);
+        }
+    }
+}
+
+fn handle_create_event(event_kind: event::CreateKind, paths: Vec<PathBuf>) {
+    for path in paths {
+        match event_kind {
+            CreateKind::File => {
+                if let Some(file_path) = path.to_str() {
+                    // cpy file to dest?
+                    copy_file_to_dest(file_path);
+                }
+            }
+            _ => {
+                println!("UNHANDLED create event {:?}", event_kind);
+            }
+        }
+    }
+}
+
+fn copy_file_to_dest(file_path: &str) {
+    println!("COPYING file {:?}", file_path);
+    let dest_path = PathBuf::from("./tests/to");
+
+    if let Some(file_name) = PathBuf::from(file_path).file_name() {
+        let dest = dest_path.join(file_name);
+        match std::fs::copy(file_path, dest) {
+            Ok(_) => {
+                println!("COPIED file to dest: {:?}", dest_path);
+            }
+            Err(e) => {
+                eprintln!("error copying file to dest: {}", e);
+            }
+        }
+    }
 }
